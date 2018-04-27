f4c2304588e9fd68d56e5daed3a9654846b935a8
vonhanap <vonhanap@mail.uc.edu>
2018-04-27 11:36:39 -0400

    incorporated bed sort

--- R/bed.sort.R
+++ R/bed.sort.R
@@ -1,9 +1,20 @@
+
+#' Bed Sort
+#'
+#' Takes a bed file, sorts it by chromosome and start, then writes it to the output location.
+#'
+#' @param bed_file_path path to the unsorted bed file.
+#' @param output_path path to the sorted bed file, the output.
+#'
+#' @return returns a bed file.
+#' @examples
+#' # bed.sort(bed_file_path = "R/locations.bed",output_path = "R/locations_sorted.bed")
 bed.sort <- function(bed_file_path,output_path){
   bedfile <- read.delim(file = bed_file_path,header = F,sep = "\t",quote = "",stringsAsFactors = F)
   bedfile <- bedfile[order(bedfile[,1],bedfile[,2]),]
   #print(bedfile)
   write.table(x = bedfile,file = output_path,append = F,quote = F,sep = "\t",row.names = F,col.names = F)
+  print(paste("wrote sorted bed file:",output_path))
 }
 
-#bed.sort(bed_file_path = "R/locations.bed",output_path = "R/locations_sorted.bed")
 
