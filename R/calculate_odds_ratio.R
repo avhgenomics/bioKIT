b0706575ff0ef988eaf259437e08df848887effd
vonhanap <vonhanap@mail.uc.edu>
2018-05-02 18:32:06 -0400

    Added odds ratio and growth rate calculations

---
+++ R/calculate_odds_ratio.R
@@ -0,0 +1,37 @@
+
+#' Calculate Odds Ratio
+#'
+#' Calculates the odds ratio of two populations and returns either a data frame or a string containing the results.
+#'
+#' @param case.positive How many in the case population that are positive for the test; integer.
+#' @param case.negative How many in the case population that are negative for the test; integer.
+#' @param control.positive How many in the control population that are positive for the test; integer.
+#' @param control.negative How many in the control population that are negative for the test; integer.
+#' @param return_df Return a dataframe instead of a string? logical.
+#' @export
+#' @return returns a string or a dataframe.
+#' @examples
+#' # ADD_EXAMPLES_HERE
+calculate.odds.ratio <- function(case.positive,case.negative,control.positive,control.negative,return_df = F){
+  odds.ratio.base <- (case.positive*control.negative) / (control.positive*case.negative)
+  upper.CI = exp(log(odds.ratio.base)+1.96*sqrt((1/case.positive)+(1/case.negative)+(1/control.positive)+(1/control.negative)))
+  lower.CI = exp(log(odds.ratio.base)-1.96*sqrt((1/case.positive)+(1/case.negative)+(1/control.positive)+(1/control.negative)))
+
+  calculated.stats <- paste("Odds Ratio:",odds.ratio.base,", 95% Confidence Interval:",lower.CI,"-",upper.CI)
+
+  if(return_df){
+    df <- data.frame(odds.ratio = odds.ratio.base,
+                     upper.CI = upper.CI,
+                     lower.CI = lower.CI)
+
+    return(df)
+  } else {return(calculated.stats)}
+
+}
+
+
+calculate.odds.ratio(case.positive = 45,
+                     case.negative = 86,
+                     control.positive = 32,
+                     control.negative = 100,return_df = T)
+
---
+++ R/growth_rate.R
@@ -0,0 +1,25 @@
+
+#' Calculate Growth Rate
+#'
+#' For cell culture.  Calculates the growth rate and doubling time of cells based on hours passed, seeding number, and current count.
+#'
+#' @param current_number number of cells counted at the end; integer.
+#' @param starting_number number of cells seeded. integer.
+#' @param time.hours number of hours that passed between seeding and collection; integer.
+#' @export
+#' @return returns a dataframe.
+#' @examples
+#' # ADD_EXAMPLES_HERE
+#'
+calculate_growth_rate <- function(current_number,starting_number,time.hours){
+  growth_rate = log(current_number / starting_number) / time.hours
+
+  dbltime = log(2)/growth_rate
+
+  values.list <- data.frame(growth_rate = growth_rate,
+                      doubling_time = dbltime)
+
+  return(values.list)
+}
+
+#calculate_growth_rate(current_number = 100,starting_number = 50,time.hours = 18)
